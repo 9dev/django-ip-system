471ea33cfd1b6dafa4bfbb4c6e2b5583172e0ec4
9dev <9devmail@gmail.com>
2016-03-11 19:22:41 +0100

    Added useful methods to the base FT.

--- fts/_base.py
+++ fts/_base.py
@@ -21,3 +21,17 @@ class BaseTestCase(StaticLiveServerTestCase):
             url = reverse(name, *args, **kwargs)
         self.browser.get('{}{}'.format(self.live_server_url, url))
 
+    def get_by_id(self, selector):
+        return self.browser.find_element_by_id(selector)
+
+    def set_field(self, field_id, value):
+        field = self.browser.find_element_by_id(field_id)
+        field.clear()
+        field.send_keys(value)
+
+    def submit(self):
+        form = self.browser.find_element_by_tag_name('form')
+        form.submit()
+
+    def get_full_url(self, url):
+        return '{}{}'.format(self.live_server_url, url)
