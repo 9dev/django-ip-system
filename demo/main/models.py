cafd23dd0902d044cbdad8ebf80bbedc375f4d05
9dev <9devmail@gmail.com>
2016-03-11 19:23:19 +0100

    Removed unnecessary author field from Article model.

--- demo/main/models.py
+++ demo/main/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=10000)
-    author = models.ForeignKey('auth.User')
+
+    def get_absolute_url(self):
+        return reverse('article_detail', kwargs={'pk': self.pk})
