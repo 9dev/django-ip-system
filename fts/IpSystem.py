d03b7f3ebd239c844a9b62325fdd15a2fe16beac
9dev <9devmail@gmail.com>
2016-03-11 18:21:38 +0100

    Created test suite schema.

---
+++ fts/IpSystem.py
@@ -0,0 +1,13 @@
+from ._base import BaseTestCase
+
+
+class TestIpSystem(BaseTestCase):
+
+    def test_can_save_and_see_object_creator_ip_if_new_ip(self):
+        self.fail()
+
+    def test_can_save_and_see_object_creator_ip_if_ip_already_known(self):
+        self.fail()
+
+    def test_can_cope_with_incorrect_ip(self):
+        self.fail()
