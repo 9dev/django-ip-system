4ab77aea299f72dc25c4c86d9537382467e197a9
9dev <9devmail@gmail.com>
2016-03-11 20:47:08 +0100

    Added saving and displaying IP of article author.

--- demo/main/views.py
+++ demo/main/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import CreateView, DetailView
 
+from ip_system.models import Ip
+
 from .forms import ArticleForm
 from .models import Article
 
@@ -7,6 +9,11 @@ from .models import Article
 class ArticleCreateView(CreateView):
     model = Article
     form_class = ArticleForm
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author_ip = Ip.get_or_create(request=self.request)
+        return super(ArticleCreateView, self).form_valid(form)
 
 
 class ArticleDetailView(DetailView):
