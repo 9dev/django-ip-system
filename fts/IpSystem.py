a2c9be1d1f79033c6d242f57e994ffce585e5ed3
9dev <9devmail@gmail.com>
2016-03-11 20:53:40 +0100

    Completed an FT for saving existing IP address.

--- fts/IpSystem.py
+++ fts/IpSystem.py
@@ -1,6 +1,11 @@
 from ._base import BaseTestCase
 
+from ip_system.models import Ip
+
 from main.models import Article
+
+
+SELENIUM_IP = '127.0.0.1'
 
 
 class TestIpSystem(BaseTestCase):
@@ -22,10 +27,29 @@ class TestIpSystem(BaseTestCase):
 
         # She can see her IP address there.
         ip_elem = self.get_by_id('id_author_ip')
-        self.assertEqual(ip_elem.text, '127.0.0.1')
+        self.assertEqual(ip_elem.text, SELENIUM_IP)
 
     def test_can_save_and_see_object_creator_ip_if_ip_already_known(self):
-        self.fail()
+        # Florence's IP address is already in the database.
+        Ip.objects.create(address=SELENIUM_IP)
+
+        # Florence hits the article create page.
+        self.get('/create')
+
+        # She fills the form.
+        self.set_field('id_title', 'MyTitle')
+        self.set_field('id_content', 'Some interesting content...')
+
+        # She submits the form.
+        self.submit()
+
+        # She is being redirected to article detail page.
+        pk = Article.objects.all().last().pk
+        self.assertEqual(self.browser.current_url, self.get_full_url('/article/{}'.format(pk)))
+
+        # She can see her IP address there.
+        ip_elem = self.get_by_id('id_author_ip')
+        self.assertEqual(ip_elem.text, SELENIUM_IP)
 
     def test_can_cope_with_incorrect_ip(self):
         self.fail()
